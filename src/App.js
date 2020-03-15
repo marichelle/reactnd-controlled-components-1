90ad2d4a8fec63e80a4a83bab20c47044559f45d
Maya Estalilla <mayaestalilla@gmail.com>
2020-03-14 23:22:35 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    mirror: ''
+  };
+
+  mirrorText(text) {
+    this.setState(prevState => ({
+      mirror: text
+    }));
+  }
+
+  render() {
+    const { mirror } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Say Something"
+            value={mirror}
+            onChange={event => this.mirrorText(event.target.value)}
+          />
+          <p className="echo">Echo: {mirror !== '' && mirror}</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
